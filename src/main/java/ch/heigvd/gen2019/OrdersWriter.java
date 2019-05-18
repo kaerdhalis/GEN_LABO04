c2ebc840fa593042240cc40601d55d55fc2951b7
kaerdhalis <benjamin.leguillou@heig-vd.ch>
2019-05-18 16:33:07 +0200

    inline de la variable product dans OrdersWriter

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -19,27 +19,26 @@ public class OrdersWriter {
             sb.append(", ");
             sb.append("\"products\": [");
             for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
 
                 sb.append("{");
                 sb.append("\"code\": \"");
-                sb.append(product.getCode());
+                sb.append(order.getProduct(j).getCode());
                 sb.append("\", ");
                 sb.append("\"color\": \"");
-                sb.append(product.getColor());
+                sb.append(order.getProduct(j).getColor());
                 sb.append("\", ");
 
-                if (product.getSize() != Size.SIZE_NOT_APPLICABLE) {
+                if (order.getProduct(j).getSize() != Size.SIZE_NOT_APPLICABLE) {
                     sb.append("\"size\": \"");
-                    sb.append(product.getSize());
+                    sb.append(order.getProduct(j).getSize());
                     sb.append("\", ");
                 }
 
                 sb.append("\"price\": ");
-                sb.append(product.getPrice());
+                sb.append(order.getProduct(j).getPrice());
                 sb.append(", ");
                 sb.append("\"currency\": \"");
-                sb.append(product.getCurrency());
+                sb.append(order.getProduct(j).getCurrency());
                 sb.append("\"}, ");
             }
 
